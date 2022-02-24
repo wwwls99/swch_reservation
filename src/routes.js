70b16ae05734181f08c9722f77850c0feb25d44f
wwwls99 <201810806@kyonggi.ac.kr>
2022-02-24 23:30:41 +0900

    [남진수] 커밋..?

--- src/routes.js
+++ src/routes.js
@@ -22,6 +22,7 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
+import Reservation from "views/examples/Reservation.js";
 
 var routes = [
   {
@@ -72,6 +73,13 @@ var routes = [
     icon: "ni ni-circle-08 text-pink",
     component: Register,
     layout: "/auth",
+  },
+  {
+    path: "/reservation",
+    name: "Reservation",
+    icon: "ni ni-circle-08 text-pink",
+    component: Reservation,
+    layout: "/admin",
   },
 ];
 export default routes;
